8339cff18f0dde8d99f9fcdc89cc371a24c0797b
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-25 17:58:12 +0100

    style configuration on base.scss

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@
 </script>
 
 <template>
-  <h1>Bonjour le monde</h1>
+  <h1 class="b1">Bonjour le monde</h1>
 </template>
 
 <style lang="scss">
 @import "./assets/base.scss";
+@import "./assets/debug.scss";
 
 </style>
