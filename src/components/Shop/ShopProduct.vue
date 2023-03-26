24437f9ce38e4c5eef16a1432953e04b66817d34
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-26 11:01:23 +0200

    style shop product

--- src/components/Shop/ShopProduct.vue
+++ src/components/Shop/ShopProduct.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="b5">
-    <h1>Shop Product</h1>
+  <div class="product d-flex flex-column">
+    <div class="product-image"></div>
+    <div class="p-10 d-flex flex-column">
+      <h4>Macbook Pro</h4>
+      <p>Lorem ipsum dolor sit amet</p>
+    </div>
+
+    <div class="d-flex flex-row align-items-center">
+      <strong class="flex-fill">Prix: 1500 $</strong>
+      <button class="btn btn-primary">Ajouter au panier</button>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.product {
+  background-color: #ffffff;
+  border: var(--border);
+  border-radius: var(--border-radius);
+  &-image {
+    border-top-right-radius: var(--border-radius);
+    border-top-left-radius: var(--border-radius);
+    background-image: url(../../assets/MacBook_Pro_14-in_Space_Gray_PDP_Image_Position-1__WWEN.webp);
+    height: 250px;
+    background-size: cover;
+    background-position: center;
+  }
+}
+</style>
